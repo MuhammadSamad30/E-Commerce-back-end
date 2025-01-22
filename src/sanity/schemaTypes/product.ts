9b1034941af2adeca9eb6cfee849adafab04d4b3
MuhammadSamad30 <romivlogs11@gmail.com>
2025-01-22 05:36:19 +0500

    E-Commerce-back-end

--- src/sanity/schemaTypes/product.ts
+++ src/sanity/schemaTypes/product.ts
@@ -1,59 +1,3 @@
-// export default {
-//   name: 'product',
-//   title: 'Product',
-//   type: 'document',
-//   fields: [
-//     {
-//       name: 'id',
-//       title: 'ID',
-//       type: 'string',
-//     },
-//     {
-//       name: 'name',
-//       title: 'Name',
-//       type: 'string',
-//     },
-//     {
-//       name: 'image',
-//       title: 'Image',
-//       type: 'image', // Use 'image' type for images
-//       options: {
-//         hotspot: true, // Enables image cropping and focal point selection
-//       },
-//     },
-//     {
-//       name: 'price',
-//       title: 'Price',
-//       type: 'number',
-//     },
-//     {
-//       name: 'description',
-//       title: 'Description',
-//       type: 'text',
-//     },
-//     {
-//       name: 'discountPercentage',
-//       title: 'Discount Percentage',
-//       type: 'number',
-//     },
-//     {
-//       name: 'isFeaturedProduct',
-//       title: 'Is Featured Product',
-//       type: 'boolean',
-//     },
-//     {
-//       name: 'stockLevel',
-//       title: 'Stock Level',
-//       type: 'number',
-//     },
-//     {
-//       name: 'category',
-//       title: 'Category',
-//       type: 'string',
-//     },
-//   ],
-// };
-
 const productSchema = {
   name: 'product',
   title: 'Product',
@@ -72,9 +16,9 @@ const productSchema = {
     {
       name: 'image',
       title: 'Image',
-      type: 'image', // Use 'image' type for images
+      type: 'image',
       options: {
-        hotspot: true, // Enables image cropping and focal point selection
+        hotspot: true, 
       },
     },
     {
